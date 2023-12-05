0ac7071d9854d9dce92aa402ce0548dd9671c416
Marek Lechowicz <m.lechow13@gmail.com>
2023-12-05 17:59:04 +0100

    Automatic db restoration and creation of web_anon role
    web_anon has acces to actor table without login

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM postgres 
 
-COPY ./backup/dvdrental.tar .
-COPY ./backup/restore.sh .
+COPY ./backup/dvdrental.tar ./dvdrental.tar
+COPY ./backup/restore.sh ./docker-entrypoint-initdb.d
 
 ENV POSTGRES_USER=postgres
 ENV POSTGRES_PASSWORD=postgres
-
-CMD ./restore.sh
--- backup/restore.sh
+++ backup/restore.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+# Recreate the database
+echo "Restoring database...";
 psql -U postgres -c "CREATE DATABASE dvdrental;";
 pg_restore -U postgres --dbname=dvdrental --verbose ./dvdrental.tar;
+echo "Database restored successfully!";
+
+# Add no-login web_anon role
+echo "Adding web_anon role...";
+psql --username postgres --dbname=dvdrental <<-EOSQL
+	CREATE ROLE web_anon WITH NOLOGIN;
+	GRANT USAGE ON SCHEMA public TO web_anon;
+    GRANT SELECT ON public.actor TO web_anon;
+EOSQL
+echo "web_anon role added successfully!";
+
