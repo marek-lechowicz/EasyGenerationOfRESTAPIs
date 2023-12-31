de58daf65c1c7dae7e95939afed8bf40d5ffead3
Marek Lechowicz <m.lechow13@gmail.com>
2023-12-31 21:12:45 +0100

    filtering\aggregating  queries

--- backup/restore.sh
+++ backup/restore.sh
@@ -7,11 +7,10 @@ pg_restore -U postgres --dbname=dvdrental --verbose ./dvdrental.tar;
 echo "Database restored successfully!";
 
 # Add no-login web_anon role
-echo "Adding web_anon role...";
+# echo "Adding web_anon role...";
 psql --username postgres --dbname=dvdrental <<-EOSQL
 	CREATE ROLE web_anon WITH NOLOGIN;
-	GRANT USAGE ON SCHEMA public TO web_anon;
-    GRANT SELECT ON public.actor TO web_anon;
+	GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO web_anon;
 EOSQL
 echo "web_anon role added successfully!";
 
